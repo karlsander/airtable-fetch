f31df119dd4a13c019f9f0e9b48a0b191567470d
Karl Sander <kall@kall.ws>
2018-05-13 18:31:22 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const airtable = require('airtable');
+
+module.exports = function AirtableFetch(baseID, baseSchema, apiKey) {
+  if (apiKey) {
+    airtable.configure({ apiKey: apiKey });
+  }
+  const base = airtable.base(baseID);
+
+  return function refresh() {
+    return new Promise(async function(resolve, reject) {
+      let data = {};
+      for (let tableName in baseSchema) {
+        data[tableName] = data[tableName] || {};
+        for (let viewName of baseSchema[tableName]) {
+          try {
+            let res =
+              await base(tableName)
+                .select({ maxRecords: 50000, view: viewName })
+                .all();
+            data[tableName][viewName] = res.map(e => e._rawJson);
+          }
+          catch (e) {
+            reject(e);
+          }
+        }
+      }
+      resolve(data);
+    });
+  };
+}
